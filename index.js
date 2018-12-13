11967088861e3e75c8045978d962972dbbbccf66
Mooradal <moorad2003@gmail.com>
2018-12-13 23:44:01 +0000

    One Page Site

--- index.js
+++ index.js
@@ -1,31 +1,3 @@
 particlesJS.load('particles-js', 'particlesjs-config2.json', function() {
 	console.log('callback - particles.js config loaded');
 });
-
-var cards = document.querySelectorAll('.card-items');
-var icons = document.querySelectorAll('.project-icon');
-
-cards[0].addEventListener('click',() => {
-	icons[0].style.animation = 'slideRight 1s ease-out 0s forwards';
-	icons[0].childNodes.querySelector('h2').style.display = 'none';
-});
-
-cards[1].addEventListener('click',() => {
-	icons[1].style.animation = 'slideRight 1s ease-out 0s forwards';
-	icons[0].childNodes.querySelector('h2').style.display = 'none';
-});
-
-cards[2].addEventListener('click',() => {
-	icons[2].style.animation = 'slideRight 1s ease-out 0s forwards';
-	icons[0].childNodes.querySelector('h2').style.display = 'none';
-});
-
-cards[3].addEventListener('click',() => {
-	icons[3].style.animation = 'slideRight 1s ease-out 0s forwards';
-	icons[0].childNodes.querySelector('h2').style.display = 'none';
-});
-
-cards[4].addEventListener('click',() => {
-	icons[4].style.animation = 'slideRight 1s ease-out 0s forwards';
-	icons[0].childNodes.querySelector('h1').style.display = 'none';
-});
